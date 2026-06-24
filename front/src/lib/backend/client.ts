28e9570582bcd6289b1486ca35fb5001c8c901b3
JUNSEO OH <piker0925@gmail.com>
2026-06-24 12:45:32 +0900

    033

--- front/src/lib/backend/client.ts
+++ front/src/lib/backend/client.ts
@@ -1,4 +1,33 @@
+import createClient from "openapi-fetch";
+
+import type { paths } from "@/lib/backend/apiV1/schema";
+
 const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const client = createClient<paths>({
+  baseUrl: NEXT_PUBLIC_API_BASE_URL,
+  credentials: "include",
+});
+
+// openapi-fetch 사용 예제
+// client
+//   .GET("/api/v1/posts/{id}", {
+//     params: {
+//       path: {
+//         id: 1,
+//       },
+//     },
+//   })
+//   .then((res) => {
+//     if (res.error) {
+//       alert(res.error.msg);
+//       return;
+//     }
+
+//     alert(`${res.data.id}번 게시물이 조회되었습니다.`);
+//   });
+
+export default client;
 
 export const apiFetch = (url: string, options?: RequestInit) => {
   options = options || {};
